3f8b35e6139d72ccfcadd270b2b01d1b91942708
Patricia Tami Sakita <67209629+tamisakita@users.noreply.github.com>
2022-07-28 19:08:23 -0300

    Initial commit

---
+++ src/test/java/com/fiapproject/droneconsumer/DroneConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiapproject.droneconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
