bdb67fcd18cdec9e01befd67524d2afd040c884a
Patricia Tami Sakita <67209629+tamisakita@users.noreply.github.com>
2022-07-28 21:59:19 -0300

    add model

--- src/main/java/com/fiapproject/droneconsumer/service/QueueConsumer.java
+++ src/main/java/com/fiapproject/droneconsumer/service/QueueConsumer.java
@@ -1,5 +1,6 @@
-package com.fiapproject.droneconsumer;
+package com.fiapproject.droneconsumer.service;
 
+import com.fiapproject.droneconsumer.model.DroneData;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -12,5 +13,10 @@ public class QueueConsumer {
     public void receive(@Payload String fileBody) {
         System.out.println("Message " + fileBody);
     }
+
+//    @RabbitListener(queues = {"${queue.name}"})
+//    public void receive(@Payload DroneData droneData) {
+//        System.out.println("Recebido! " + droneData);
+//    }
 
 }
